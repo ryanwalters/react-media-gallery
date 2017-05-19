2035de8e3299bb2040beb1d8a6ec9a51a8d66ae7
Ryan Walters <rwalters@eb.com>
2017-05-19 16:46:19 -0500

    add props: ad, onClose

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,15 +1,49 @@
-import React from 'react'
+import React, { Component } from 'react'
 import {render} from 'react-dom'
 
-import Component from '../../src/MediaGallery'
+import MediaGallery from '../../src/MediaGallery'
 
-let Demo = React.createClass({
-  render() {
-    return <div>
-      <h1>react-media-gallery Demo</h1>
-      <Component/>
-    </div>
-  }
-})
+class Demo extends Component {
 
-render(<Demo/>, document.querySelector('#demo'))
+    constructor() {
+
+        super();
+
+        this.state = {
+            visible: true
+        };
+
+        this.openGallery = this.openGallery.bind(this);
+        this.closeGallery = this.closeGallery.bind(this);
+    }
+
+    render() {
+
+        return (
+            <div>
+                <h1>react-media-gallery Demo</h1>
+                <button onClick={this.openGallery}>Open Gallery</button>
+                <MediaGallery
+                    style={{display: this.state.visible ? '' : 'none'}}
+                    onClose={this.closeGallery}
+                />
+            </div>
+        );
+    }
+
+    openGallery() {
+
+        this.setState({
+            visible: true
+        });
+    }
+
+    closeGallery() {
+
+        this.setState({
+            visible: false
+        });
+    }
+}
+
+render(<Demo/>, document.querySelector('#demo'));
--- src/MediaGallery.js
+++ src/MediaGallery.js
@@ -4,34 +4,50 @@ import 'britannica-styleguide/src/layout/_layout.scss';
 import './MediaGallery.scss';
 
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+const setDisplay = (value) => ({ display: value ? '' : 'none' });
 
 class MediaGallery extends Component {
 
     render() {
 
         return (
-            <div className="eb-gallery grid no-gutter">
+            <div className="eb-gallery grid no-gutter" style={this.props.style}>
                 <div className="grid-xs">
-                    <div className="eb-gallery-container grid no-gutter" data-layout="column" style={{backgroundColor: '#222'}}>
+                    <div className="eb-gallery-container grid no-gutter" data-layout="column">
                         <div className="eb-gallery-media grid-xs middle-xs center-xs">
                             <img src="http://placehold.it/1600x900/555555/ffffff"/>
                         </div>
-                        <div className="eb-gallery-description grid-xs-shrink" style={{backgroundColor: '#333', minHeight: '200px'}}>
+                        <div className="eb-gallery-description grid-xs-shrink">
                         </div>
-                        <div className="eb-gallery-slides grid-xs-shrink" style={{backgroundColor: '#444', minHeight: '100px'}}>
+                        <div className="eb-gallery-slides grid-xs-shrink">
                         </div>
                     </div>
                 </div>
                 <div className="grid-xs-shrink">
-                    <div className="eb-gallery-ads middle-xs" style={{display: 'none'}}>
-                        <img src="http://placehold.it/160x600"/>
+                    <div className="eb-gallery-ads middle-xs" style={setDisplay(this.props.ad)}>
+                        <img src="http://placehold.it/300x250"/>
                     </div>
                 </div>
                 <div className="eb-gallery-controls">
+                    <div className="eb-gallery-close" onClick={this.props.onClose} style={setDisplay(this.props.onClose)}>
+                        &times;
+                    </div>
                 </div>
             </div>
-        )
+        );
     }
 }
+
+MediaGallery.propTypes = {
+    ad: PropTypes.object,
+    onClose: PropTypes.func
+};
+
+MediaGallery.defaultProps = {
+    ad: null,
+    onClose: null
+};
 
 export default MediaGallery;
